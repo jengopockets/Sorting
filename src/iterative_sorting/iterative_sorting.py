99cb64de93e3d91fa8cd3ccaa43a3a5c8f0032c3
Jengopockets <jengopockets@gmail.com>
2020-03-18 15:38:31 -0400

    MVp

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -6,23 +6,44 @@ def selection_sort( arr ):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
+        for num in range(cur_index, len(arr)):
+            if arr[num] < arr[smallest_index]:
+                smallest_index = num
              
 
 
 
         # TO-DO: swap
+        temp = arr[smallest_index]
+        arr[smallest_index] = arr[cur_index]
+        arr[cur_index] = temp
 
 
 
 
     return arr
+arr_sort = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+# print(selection_sort(arr_sort))
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
+    swap = True
+    while swap:
+        swap = False
+        for i in range(0, len(arr) - 1):
+            if arr[i] > arr[i+1]:
+
+                #swap point
+                temp = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = temp
+                swap = True
 
     return arr
 
+
+print(bubble_sort(arr_sort))
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -2,18 +2,54 @@
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
+    #loop through index of our added array size
+    for i in range(0, elements):
+        #Check first possition in both arrays
+        if arrA == []: #Checks to see if array A is empty
+            merged_arr[i] = arrB[0]
+            arrB.remove(arrB[0])
+        elif arrB == []: #Checks if array B is empty
+            merged_arr[i] = arrA[0]
+            arrA.remove(arrA[0])
+        elif arrA[0] < arrB[0]:
+            #make merged_arr index equal lowest variable
+            merged_arr[i] = arrA[0]
+            #remove index from original array
+            arrA.remove(arrA[0])
+
+        else:
+            merged_arr[i] = arrB[0]
+            arrB.remove(arrB[0])
+
     # TO-DO
     
     return merged_arr
 
-
 # TO-DO: implement the Merge Sort function below USING RECURSION
+#Split list function
+def split(a_list):
+    if len(a_list) == 1:
+        pass
+    else:
+        half = len(a_list)//2
+        return a_list[ 0 :half], a_list[half:]
 def merge_sort( arr ):
+    # elements = len(arr) #checks for elements in array
+    # sorted_list = []
+    
+    if len(arr) > 1:
+        A, B = split(arr)
+        left = merge_sort(A)
+        right = merge_sort(B)
+        arr = merge(left, right)
+    
+
+    
     # TO-DO
 
     return arr
 
-
+print(merge_sort([1, 5, 8, 4, 2, 9, 6, 0, 3, 7]))
 # STRETCH: implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
     # TO-DO
